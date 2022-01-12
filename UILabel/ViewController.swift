2b951b5b86f8276f29f324845c8753df02a7e86c
Dastan <khalilovdastan@gmail.com>
2022-01-12 16:38:35 +0600

    Initial Commit

---
+++ UILabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by Дастан Халилов on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
